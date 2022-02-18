07e5fd090f0f94d8fe8ae70c2fa53571ac881245
Zach Meyer <zachary.michael.meyer@gmail.com>
2022-02-18 12:24:44 -0500

    Fix Fibonacci to be correct

--- 01_fibonacci.rb
+++ 01_fibonacci.rb
@@ -2,20 +2,23 @@
 
 def fibs(number)
   fib_array = [0]
-  (number + 1).times do |num|
+  (number).times do |num|
     if num == 1
       fib_array.push(1)
     elsif num > 1
       fib_array.push(fib_array[num - 1] + fib_array[num - 2])
     end
   end
-  fib_array.last
+  fib_array
 end
 
-puts fibs(10) # Returns 55
+p fibs(10) # Returns [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
-def fibs_rec(number)
-  number < 2 ? number : fibs_rec(number - 1) + fibs_rec(number - 2)
+def fibs_rec(number, array = [0, 1])
+  return array.first(number) if array.length >= number
+  array << array[-1] + array[-2]
+  fibs_rec(number, array)
+  # number < 2 ? number : fibs_rec(number - 1) + fibs_rec(number - 2) - THIS IS WRONG HAHA
 end
 
-puts fibs_rec(10) # Returns 55
+p fibs_rec(10) # Returns [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
